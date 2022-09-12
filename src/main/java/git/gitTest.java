b4deea7404d29306ed659f92600b3f237187a03e
Serein <3097143698@qq.com>
2022-09-12 22:34:55 +0800

    first commit

---
+++ src/main/java/git/gitTest.java
@@ -0,0 +1,12 @@
+package git;
+
+/**
+ * @Description:
+ * @author: You_are_infinite
+ * @create: 2022-09-12 22:30
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

428fc9d1cd6eea2ff88c6b5057ff36348262edc2
Serein <3097143698@qq.com>
2022-09-13 10:59:59 +0800

    Push commit

--- src/main/java/git/gitTest.java
+++ src/main/java/git/gitTest.java
@@ -13,5 +13,7 @@ public class gitTest {
         System.out.println("hello git4");
         System.out.println("hello master test");
         System.out.println("hello fix test");
+        System.out.println("push test!");
+
     }
 }
